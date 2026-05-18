54accdb204feb624f89d47b9e704f41f0fd3b190
Yohannes <y.berhane56@gmail.com>
2026-05-17 22:51:10 -0400

    Remove old chapter 2

--- src/main/scala/chapter2/exercises/ex4.scala
+++
@@ -1,7 +0,0 @@
-package chapter2.exercises
-
-/** Reverse a curried function */
-@main def implementUncurry: Unit =
-
-  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
-    (a, b) => f(a)(b)
--- src/main/scala/chapter2/exercises/ex5.scala
+++
@@ -1,6 +0,0 @@
-package chapter2.exercises
-
-/** feeds the output of one function to the input of another function */
-@main def implementFunctionComposition: Unit =
-  def compose[A, B, C](f: B => C, g: A => B): A => C =
-    a => f(g(a))
