04807d3e850d2004c6d9d6cd9aaef0df26642cbe
Yohannes <y.berhane56@gmail.com>
2026-05-23 03:32:07 +0000

    Ex.12 List.reverse

--- src/main/scala/datastructures/List.scala
+++ src/main/scala/datastructures/List.scala
@@ -38,6 +38,9 @@ object List:
   def length[A](as: List[A]): Int =
     foldLeft(as, 0, (acc, _) => acc + 1)
 
+  def reverse[A](as: List[A]): List[A] =
+    foldLeft(as, Nil, (acc, a) => Cons(a, acc))
+
   def foldRight[A, B](as: List[A], acc: B, f: (A, B) => B): B =
     as match
       case Nil         => acc
