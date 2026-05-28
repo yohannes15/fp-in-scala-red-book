e59827c216bf21f7b6a4d89a4365188bd8323c57
Yohannes <y.berhane56@gmail.com>
2026-05-28 08:36:58 +0000

    map2 exercise impl

--- src/main/scala/random/RNG.scala
+++ src/main/scala/random/RNG.scala
@@ -66,7 +66,10 @@ case class SimpleRNG(seed: Long) extends RNG:
     map(nonNegativeInt)(i => i - (i % 2))
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    ???
+    rng0 =>
+      val (a, rng1) = ra(rng0)
+      val (b, rng2) = rb(rng1)
+      (f(a, b), rng2)
 
   def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
     map2(ra, rb)((_, _))
