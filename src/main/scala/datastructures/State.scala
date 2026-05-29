44de30e587fe90b2568758249c835abfaf3f850e
Yohannes <y.berhane56@gmail.com>
2026-05-29 03:33:36 +0000

    initial state opaque type representing generic stateful programs

---
+++ src/main/scala/datastructures/State.scala
@@ -0,0 +1,9 @@
+package datastructures
+
+opaque type State[S, +A] = S => (A, S)
+
+object State:
+  extension [S, A](underlying: State[S, A])
+    def run(s: S): (A, S) = underlying(s)
+
+  def apply[S, A](f: S => (A, S)): State[S, A] = f
